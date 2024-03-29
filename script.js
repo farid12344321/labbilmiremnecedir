f3d907eca61047a31ca2eb228d1785826c1226e6
Farid R. Abdullayev <faridrab@code.local>
2024-03-29 21:29:19 +0400

    lab2

--- script.js
+++ script.js
@@ -26,12 +26,60 @@ function getInputCount() {
 
 document.getElementById("registerForm").addEventListener("submit", function (e) {
     e.preventDefault();
+
+    let spans = document.querySelectorAll("span")
+    spans.forEach(elem=>elem.remove())
+
+
     let listul = document.querySelector(".list-ul");
+    let inputsul =  document.querySelectorAll(".list-input input");
+    let error1 = document.querySelector(".error")
+    let isempty = false;
+
+    inputsul.forEach(element => {
+        if(element.value.trim() ===""){
+            isempty = true
+        }
+    });
+    if(isempty){
+        let span = showError("Please fill in all input fields");
+        error1.appendChild(span);
+        return;
+    }
+
+    let h1 = document.querySelector("h1");
+    h1.style.display = "block"
     listul.innerHTML = "";
     document.querySelectorAll(".list-input input").forEach((elem, index) => {
         listul.appendChild(createLl(elem,index))
     })
 })
+
+
+
+function showError(message){
+    let span = document.createElement("span");
+    span.innerText = message;
+    span.style.color="red";
+    span.style.border = "solid 2px black"
+   
+    setTimeout(function(){
+        span.remove();
+    },3000)
+
+    return span;
+}
+
+
+
+
+
+
+
+
+
+
+
 function createLl(elem,index) {
     let li = document.createElement("li");
     li.style.listStyle = "none";
@@ -40,7 +88,6 @@ function createLl(elem,index) {
     li.textContent = (index+1)+". " +elem.value
     return li;
 }
-
 
 
 
